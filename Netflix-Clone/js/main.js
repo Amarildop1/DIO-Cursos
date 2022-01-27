11dfa9612cb7df93feb524b4d0c6b4de8377a4ff
Amarildo <amarildop1dev@gmail.com>
2022-01-27 01:13:37 -0300

    Adicionado vida aos botões, título nas seções e um rodapé

--- Netflix-Clone/js/main.js
+++ Netflix-Clone/js/main.js
@@ -15,3 +15,11 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
+
+function abrirNetflix(){
+    window.open("https://www.netflix.com/br/title/70177057", "_blank");
+}
+
+function abrirInformacoes(){
+    window.open("https://pt.wikipedia.org/wiki/The_Walking_Dead_(s%C3%A9rie_de_televis%C3%A3o)", "_blank");
+}
