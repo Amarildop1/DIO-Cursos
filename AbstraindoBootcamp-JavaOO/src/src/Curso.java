ca4551c794d1db52dc06db2ad9ee9c29b13970c0
Amarildo <amarildop1dev@gmail.com>
2022-03-03 21:00:28 -0300

    Abstração de um Bootcamp | Java e Orientação à Objetos

--- AbstraindoBootcamp-JavaOO/src/src/Curso.java
+++ AbstraindoBootcamp-JavaOO/src/src/Curso.java
@@ -2,5 +2,41 @@ package src;
 
 
 public class Curso {
+    private String titulo;
+    private String descricao;
+    private int cargaHoraria;
     
-}
+    public Curso(){
+    
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso: " + titulo + "\nDescricao: " + descricao + "\nCarga Horária: " + cargaHoraria + "\n";
+    }
+    
+    
+    
+    
+} //Final da class Curso
--- AbstraindoBootcamp-JavaOO/src/src/Principal.java
+++ AbstraindoBootcamp-JavaOO/src/src/Principal.java
@@ -1,8 +1,31 @@
 package src;
 
+import java.time.LocalDate;
+
 
 public class Principal {
     public static void main(String[] args) {
         
-    }
-}
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Programação Orientada à Objetos com Java");
+        curso1.setDescricao("Aulas de Java atualizadas");
+        curso1.setCargaHoraria(40);
+        
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Javascript e POO");
+        curso2.setDescricao("Aulas de Javascript");
+        curso2.setCargaHoraria(40);
+        
+        System.out.println("\n=============== Bootcamp ===============");
+        System.out.println(curso1);
+        System.out.println(curso2);
+        
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Entrando no mercado com Java");
+        mentoria.setDescricao("Programação com Java");
+        mentoria.setData(LocalDate.now());
+        
+        System.out.println(mentoria);
+        
+    }//Final do método main
+}//Final da class Principal
