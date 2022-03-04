e6deb70770630f9650ac1f8264581043419250db
Amarildo <amarildop1dev@gmail.com>
2022-03-03 22:40:09 -0300

    Adicionando classe mãe para usar herança

---
+++ AbstraindoBootcamp-JavaOO/src/src/Conteudo.java
@@ -0,0 +1,30 @@
+package src;
+
+
+public abstract class Conteudo {
+    
+    protected static final int XP_PADRAO = 20;
+    
+    private String titulo;
+    private String descricao;
+    
+    public abstract int calcularXP();
+
+    
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    
+    
+}//Final da class Conteudo
